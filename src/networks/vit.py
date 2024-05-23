4ae3d44e3325c425390a505c41f09a7bf65d5269
Ayo Ore <ore@thphys.uni-heidelberg.de>
2024-05-23 12:14:35 +0200

    Add patch masking and gradient checkpointing to ViT.

--- src/networks/vit.py
+++ src/networks/vit.py
@@ -4,10 +4,9 @@ import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.utils.checkpoint as cp
-from einops import rearrange
+from einops import rearrange, repeat
 from timm.models.vision_transformer import Attention, Mlp
-
+from torch.utils.checkpoint import checkpoint
 
 class ViT(nn.Module):
     """
@@ -20,50 +19,49 @@ class ViT(nn.Module):
 
         self.cfg = cfg
         in_channels, *axis_sizes = cfg.in_shape
+        dim = cfg.hidden_dim
 
-        # check shapes
-        for i, (s, p) in enumerate(zip(axis_sizes, cfg.patch_shape)):
-            assert not s % p, \
-                f"Input size ({s}) should be divisible by patch size ({p}) in axis {i}."
-        assert not cfg.hidden_dim % 6, \
-            f"Hidden dim should be divisible by 6 (for fourier position embeddings)"
+        # check consistency of arguments
+        check_shapes(cfg)
         
-        # initialize x,t,c embeddings
+        # embedding layer
         patch_dim = math.prod(cfg.patch_shape) * in_channels
-        self.x_embedder = nn.Linear(patch_dim, cfg.hidden_dim)
-        self.num_patches = [s // p for s, p in zip(axis_sizes, cfg.patch_shape)]
-        
-        # initialize fourier frequencies for position embeddings
-        fourier_dim = cfg.hidden_dim // 6
-        w = torch.arange(fourier_dim) / (fourier_dim - 1)
-        w = 1. / (10_000 ** w)
-        w = w.repeat(3)
-        self.pos_encoding_freqs = nn.Parameter(
-            w.log() if cfg.learn_pos_encoding else w,
-            requires_grad=cfg.learn_pos_encoding
-        )
+        self.embedding = nn.Linear(patch_dim, dim)
 
-        # initialize coordinate grids for position embeddings
-        for i, n in enumerate(self.num_patches):
+        # position encoding
+        fourier_dim = dim // 6 # sin/cos features for each dime
+        w = torch.arange(fourier_dim) / (fourier_dim - 1)
+        w = (1. / (10_000 ** w)).repeat(3)
+        self.pos_encoding_freqs = nn.Parameter(
+            w.log() if cfg.learn_pos_encoding else w, requires_grad=cfg.learn_pos_encoding
+        )
+        self.num_patches = [s // p for s, p in zip(axis_sizes, cfg.patch_shape)]
+        for i, n in enumerate(self.num_patches): # axis values for each dim
             self.register_buffer(f'grid_{i}', torch.arange(n)*(2*math.pi/n))
 
-        # initialize transformer stack
+        # transformer stack
         self.blocks = nn.ModuleList([
             Block(
-                cfg.hidden_dim, cfg.num_heads, mlp_ratio=cfg.mlp_ratio, mlp_drop=cfg.mlp_drop,
-                attn_drop=cfg.attn_drop, proj_drop=cfg.proj_drop
+                dim, cfg.num_heads, mlp_ratio=cfg.mlp_ratio, mlp_drop=cfg.mlp_drop,
+                checkpoint_grads=cfg.checkpoint_grads, attn_drop=cfg.attn_drop,
+                proj_drop=cfg.proj_drop
             ) for _ in range(cfg.depth)
         ])
 
-        # initialize output layer
-        # if cfg.final_conv: # TODO: Is final conv sensible for classification?
-        #     final_conv_channels = cfg.final_conv_channels or in_channels
-        #     self.final_layer = FinalLayer(cfg.hidden_dim, cfg.patch_shape, final_conv_channels)
-        #     self.conv_layer = nn.Conv3d(final_conv_channels, in_channels, kernel_size=3, padding=1)
-        # else:
-        self.final_layer = FinalProj(
-            cfg.hidden_dim, cfg.patch_shape, cfg.out_channels, cfg.out_act, cfg.mlp_drop
+        # output head
+        self.head = nn.Sequential(
+            nn.LayerNorm(dim, elementwise_affine=False, eps=1e-6),
+            nn.Linear(dim, dim),
+            nn.ReLU(),
+            nn.Dropout(cfg.mlp_drop),
+            nn.Linear(dim, cfg.out_channels)
         )
+        self.out_act = getattr(F, cfg.out_act) if cfg.out_act else nn.Identity()
+
+        # masking
+        if cfg.mask_frac > 0:
+            assert cfg.mask_frac < 1
+            self.mask_token = nn.Parameter(torch.randn(dim))
 
     def pos_encoding(self): # TODO: Simplify for fixed dim=3
         grids = [getattr(self, f'grid_{i}') for i in range(3)]
@@ -80,19 +78,25 @@ class ViT(nn.Module):
         ]
         return torch.cat(features, dim=1)
 
-    def forward(self, x):
+    def forward(self, x, mask=False):
         """
         Forward pass of ViT.
         x: (B, C, *axis_sizes) tensor of spatial inputs
+        x: whether or not to 
         """
-        
-        x = self.to_patches(x)                       # (B, T, D), where T = prod(num_patches)
-        x = self.x_embedder(x) + self.pos_encoding() # (B, T, D)
-        for block in self.blocks:
-            x = block(x)                             # (B, T, D)
-        x = self.final_layer(x)                      # (B, T, prod(patch_shape) * out_channels)
 
-        return x
+        x = self.to_patches(x) # (B, T, D), with T = prod(num_patches)
+        x = self.embedding(x)
+        if mask:
+            self.random_mask_patches(x)
+        x = x + self.pos_encoding() # TODO: Check whether masked tokens should really get a position embedding
+
+        for block in self.blocks:
+            x = block(x)
+
+        x = torch.mean(x, axis=1) # (B, D)
+        x = self.head(x) # (B, Z)
+        return self.out_act(x) 
 
     def to_patches(self, x):
         x = rearrange(
@@ -100,46 +104,49 @@ class ViT(nn.Module):
             **dict(zip(('p1', 'p2', 'p3'), self.cfg.patch_shape))
         )
         return x
+
+    def random_mask_patches(self, x):
     
+        if not self.cfg.mask_frac:
+            print("WARNING: Option `mask_frac` is zero. No masking will be applied.")
+            return x
+    
+        B, T = x.size(0), math.prod(self.num_patches)
+        num_masked = int(self.cfg.mask_frac * T)
+        full_mask = repeat(self.mask_token, 'd -> b t d', b=B, t=T)
+        mask_idcs = torch.rand(B, T, device=x.device).topk(k=num_masked, dim=-1).indices
+        mask_map = torch.zeros((B, T), device=x.device).scatter_(-1, mask_idcs, 1).bool()
+
+        return torch.where(mask_map[..., None], full_mask, x)
 
 class Block(nn.Module):
-    def __init__(self, hidden_size, num_heads, mlp_ratio=4.0, mlp_drop=0., **block_kwargs):
+    def __init__(
+            self, hidden_size, num_heads, mlp_ratio=4.0, mlp_drop=0., checkpoint_grads=False,
+            **attn_kwargs
+        ):
         super().__init__()
         self.norm1 = nn.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
-        self.attn = Attention(hidden_size, num_heads=num_heads, qkv_bias=True, **block_kwargs)
+        self.attn = Attention(hidden_size, num_heads=num_heads, qkv_bias=True, **attn_kwargs)
         self.norm2 = nn.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
         mlp_hidden_dim = int(hidden_size * mlp_ratio)
         approx_gelu = lambda: nn.GELU(approximate="tanh")
         self.mlp = Mlp(
-            in_features=hidden_size, hidden_features=mlp_hidden_dim,
-            act_layer=approx_gelu, drop=mlp_drop
+            in_features=hidden_size, hidden_features=mlp_hidden_dim, act_layer=approx_gelu,
+            drop=mlp_drop
         )
+        self.checkpoint_grads = checkpoint_grads
 
     def forward(self, x):
-        x = x + self.attn(self.norm1(x))
+        if self.checkpoint_grads:
+            x = x + checkpoint(self.attn, self.norm1(x), use_reentrant=False)
+        else:
+            x = x + self.attn(self.norm1(x))
         x = x + self.mlp(self.norm2(x))
         return x
-
-
-class FinalProj(nn.Module):
-    def __init__(self, hidden_dim, patch_shape, out_channels, act=None, drop=0.):
-        super().__init__()
-        self.proj1 = nn.Sequential(
-            nn.LayerNorm(hidden_dim, elementwise_affine=False, eps=1e-6),
-            nn.Linear(hidden_dim, hidden_dim),
-            nn.Dropout(drop)
-        )
-        self.proj2 = nn.Sequential(
-            nn.LayerNorm(hidden_dim, elementwise_affine=False, eps=1e-6),
-            nn.Linear(hidden_dim, hidden_dim),
-            nn.ReLU(),
-            nn.Dropout(drop),
-            nn.Linear(hidden_dim, out_channels)
-        )
-        self.act = getattr(F, act) if act else nn.Identity()
-
-    def forward(self, x):
-        x = self.proj1(x)
-        x = torch.mean(x, axis=1)
-        x = self.proj2(x)
-        return self.act(x)
+    
+def check_shapes(cfg):
+    for i, (s, p) in enumerate(zip(cfg.in_shape[1:], cfg.patch_shape)):
+        assert not s % p, \
+            f"Input size ({s}) should be divisible by patch size ({p}) in axis {i}."
+    assert not cfg.hidden_dim % 6, \
+        f"Hidden dim should be divisible by 6 (for fourier position embeddings)"    
