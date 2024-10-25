abd5368f4d40e4726429765eb636b2756e6c043e
Ayo Ore <ore@thphys.uni-heidelberg.de>
2024-10-25 11:18:43 +0200

    Remove CFM

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,4 @@
 from .classifier import Classifier
-from .flow_matcher import ConditionalFlowMatcher
 from .jepa import JEPA
 from .npe import NPE, CalibratedNPE
 from .regressor import Regressor, GaussianRegressor
